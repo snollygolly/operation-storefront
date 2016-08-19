1066da58560b15afaeec80012bbe60e46860a7a4
snollygolly <snollygolly@gmail.com>
2016-08-18 23:19:05 -0500

    built out basic page structure

--- controllers/main.js
+++ controllers/main.js
@@ -7,3 +7,33 @@ module.exports.index = function* index() {
 		title: config.site.name
 	});
 };
+
+module.exports.aboutUs = function* aboutUs() {
+	yield this.render("about_us", {
+		title: config.site.name
+	});
+};
+
+module.exports.contactUs = function* contactUs() {
+	yield this.render("contact_us", {
+		title: config.site.name
+	});
+};
+
+module.exports.jobs = function* jobs() {
+	yield this.render("jobs", {
+		title: config.site.name
+	});
+};
+
+module.exports.signUp = function* signUp() {
+	yield this.render("sign_up", {
+		title: config.site.name
+	});
+};
+
+module.exports.login = function* login() {
+	yield this.render("login", {
+		title: config.site.name
+	});
+};
--- routes.js
+++ routes.js
@@ -11,7 +11,11 @@ const routes = new Router();
 const main = require("./controllers/main.js");
 
 // routes
-
 routes.get("/", main.index);
+routes.get("/about_us", main.aboutUs);
+routes.get("/contact_us", main.contactUs);
+routes.get("/jobs", main.jobs);
+routes.get("/sign_up", main.signUp);
+routes.get("/login", main.login);
 
 app.use(routes.middleware());
