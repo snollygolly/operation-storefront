380b99cc59e787bd3e6fd81d86482f2dc7bf1e36
snollygolly <snollygolly@gmail.com>
2016-09-04 13:32:39 -0500

    added more canon code comments

--- assets/js/secure/questions.js
+++ assets/js/secure/questions.js
@@ -1,3 +1,8 @@
+// RP: this is fucking creepy, and i don't like it
+// JD: rick, i've been here for long enough to kind of figure out how things work.  don't ask questions
+// RP: yeah, but john, someone has to, don't they?
+// JD: don't ask question rick, i'm serious
+
 $(document).ready(function() {
 
 	$("#sf-submit").on("click", function(e) {
